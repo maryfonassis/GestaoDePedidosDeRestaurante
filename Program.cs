cf6938287ea48eee5a95ed7a91598480ec019a0a
Mariany <marianyf76@gmail.com>
2026-06-13 19:35:14 -0300

    Atualização final

--- Program.cs
+++ Program.cs
@@ -6,29 +6,32 @@ using GestaoDePedidoDeRestaurante.Servicos;
 Queue<Pedido> filaPedido = new Queue<Pedido>();
 Stack<Pedido> pedidosCancelados = new Stack<Pedido>();
 Cliente[] clientes = new Cliente[50];
+
+bool[,] mesas = new bool[5,5];
+
 var cardapio = CardapioService.CarregarCardapio();
 int indiceClientes = 0;
 int codigo = 111;
-
 
 try
 {
     bool rodar = true;
     while (rodar)
     {
-        
         Console.WriteLine("*******RESTAURANTE******* ");
-            Console.WriteLine("1 - Novo Pedido Delivery");
-            Console.WriteLine("2 - Novo Pedido Presencial");
-            Console.WriteLine("3 - Mostrar Fila");
-            Console.WriteLine("4 - Atender Pedido");
-            Console.WriteLine("5 - Cancelar Pedido");
-            Console.WriteLine("6 - Mostrar Cancelados");
-            Console.WriteLine("7 - Mostrar Clientes");
-            Console.WriteLine("8 - Mostrar Cardápio");
-            Console.WriteLine("9 - Sair");
+        Console.WriteLine("1 - Novo Pedido Delivery");
+        Console.WriteLine("2 - Novo Pedido Presencial");
+        Console.WriteLine("3 - Mostrar Fila");
+        Console.WriteLine("4 - Atender Pedido");
+        Console.WriteLine("5 - Cancelar Pedido");
+        Console.WriteLine("6 - Mostrar Cancelados");
+        Console.WriteLine("7 - Mostrar Clientes");
+        Console.WriteLine("8 - Mostrar Cardápio");
+        Console.WriteLine("9 - Mostrar Mesas");
+        Console.WriteLine("10 - Pedidos por Valor Total");
+        Console.WriteLine("11 - Sair");
 
-        System.Console.Write("Escolha uma opção: ");
+        Console.Write("Escolha uma opção: ");
         int opcao = int.Parse(Console.ReadLine()!);
 
         switch(opcao)
@@ -39,132 +42,143 @@ try
 
                 Console.Write("Telefone: ");
                 string telefone = Console.ReadLine()!;
+
                 Cliente cliente = new(nome, telefone);
-                clientes[indiceClientes] = cliente;
-                indiceClientes++;
+                clientes[indiceClientes++] = cliente;
 
                 Console.Write("CEP: ");
                 string cep = Console.ReadLine()!;
+
                 Endereco endereco = await LocalizacaoService.BuscarEndereco(cep);
                 endereco.PreencherEndereco();
 
-                Pedido delivery = new PedidoDelivery(cliente, codigo, endereco);
-                codigo++;
-    
+                Pedido delivery = new PedidoDelivery(cliente, codigo++, endereco);
+
                 Console.Write("Digite o codigo do item: ");
                 int codigoItem = int.Parse(Console.ReadLine()!);
-                var pedido = CardapioService.BuscarItemPorCodigo(cardapio, codigoItem);
-                delivery.AdicionarPedido(pedido);
-                filaPedido.Append(delivery);
+
+                var item = CardapioService.BuscarItemPorCodigo(cardapio, codigoItem);
+                delivery.AdicionarPedido(item);
+
+                filaPedido.Enqueue(delivery);
                 Console.WriteLine("Pedido Feito!");
                 break;
-            case 2:
 
+            case 2:
                 Console.Write("Nome do cliente: ");
                 nome = Console.ReadLine()!;
 
                 Console.Write("Telefone: ");
                 telefone = Console.ReadLine()!;
-                Cliente clientePresencial = new(nome, telefone);
-                clientes[indiceClientes] = clientePresencial;
-                indiceClientes++;
-                System.Console.Write("Número da mesa: ");
 
-                int mesa = int.Parse(Console.ReadLine()!);
-                Pedido presencial = new PedidoPresencial(clientePresencial, codigo, mesa);
-                codigo++;
-    
+                Cliente clientePresencial = new(nome, telefone);
+                clientes[indiceClientes++] = clientePresencial;
+
+                Console.Write("Linha da mesa (0-4): ");
+                int linha = int.Parse(Console.ReadLine()!);
+
+                Console.Write("Coluna da mesa (0-4): ");
+                int coluna = int.Parse(Console.ReadLine()!);
+
+                if (mesas[linha,coluna])
+                {
+                    Console.WriteLine("Mesa ocupada!");
+                    break;
+                }
+
+                mesas[linha,coluna] = true;
+
+                Pedido presencial = new PedidoPresencial(clientePresencial, codigo++, (linha * 5) + coluna + 1);
+
                 Console.Write("Digite o codigo do item: ");
                 codigoItem = int.Parse(Console.ReadLine()!);
-                pedido = CardapioService.BuscarItemPorCodigo(cardapio, codigoItem);
-                presencial.AdicionarPedido(pedido);
-                filaPedido.Append(presencial);
+
+                item = CardapioService.BuscarItemPorCodigo(cardapio, codigoItem);
+                presencial.AdicionarPedido(item);
+
+                filaPedido.Enqueue(presencial);
                 Console.WriteLine("Pedido presencial registrado.");
                 break;
 
             case 3:
-
-                Console.WriteLine(
-                    "**** FILA ****");
-
+                Console.WriteLine("**** FILA DE ATENDIMENTO ****");
                 foreach(var pedidoFila in filaPedido)
                 {
-                    Console.WriteLine(
-                        $"Código: {pedidoFila.Codigo}");
-
-                    Console.WriteLine(
-                        $"Cliente: {pedidoFila.Cliente.Nome}");
-
-                    Console.WriteLine(
-                        $"Total: {pedidoFila.Total}");
-
+                    Console.WriteLine($"Código: {pedidoFila.Codigo}");
+                    Console.WriteLine($"Cliente: {pedidoFila.Cliente.Nome}");
+                    Console.WriteLine($"Total: {pedidoFila.CalcularTotal()}");
                     Console.WriteLine("-----------");
-                 }
+                }
+                break;
 
-                 break;
             case 4:
                 if(filaPedido.Count > 0)
                 {
-                    Pedido pedidoAtendido =
-                    filaPedido.Dequeue();
-
-                    Console.WriteLine(
-                    "Pedido atendido!");
-
-                Console.WriteLine(
-                  $"Cliente: " +
-                 $"{pedidoAtendido.Cliente.Nome}");
-
-                Console.WriteLine(
-                     $"Código: " +
-                 $"{pedidoAtendido.Codigo}");
+                    Pedido pedidoAtendido = filaPedido.Dequeue();
+                    Console.WriteLine("Pedido atendido!");
+                    Console.WriteLine($"Cliente: {pedidoAtendido.Cliente.Nome}");
+                    Console.WriteLine($"Código: {pedidoAtendido.Codigo}");
                 }
-
                 else
                 {
-                    Console.WriteLine(
-                        "Fila vazia!");
+                    Console.WriteLine("Fila vazia!");
                 }
                 break;
+
             case 5:
-                System.Console.WriteLine("Digite o codigo do pedido: ");
+                Console.Write("Digite o codigo do pedido: ");
                 int pedidoCodigo = int.Parse(Console.ReadLine()!);
-                pedidosCancelados.Push(filaPedido.FirstOrDefault(pedido => pedido.Codigo == pedidoCodigo));
-                var filaPedidoTemp = new Queue<Pedido>(filaPedido.Where(pedido => pedido.Codigo != pedidoCodigo));
-                filaPedido = filaPedidoTemp;
+
+                var cancelado = filaPedido.FirstOrDefault(p => p.Codigo == pedidoCodigo);
+
+                if(cancelado != null)
+                {
+                    pedidosCancelados.Push(cancelado);
+                    filaPedido = new Queue<Pedido>(filaPedido.Where(p => p.Codigo != pedidoCodigo));
+                }
                 break;
 
             case 6:
-                Console.WriteLine("Pedidos cancelados:");
-                foreach (var item in pedidosCancelados)
-                {
-                   item.ExibirResumo();
-                }
+                Console.WriteLine("Pedidos cancelados (pilha):");
+                foreach (var p in pedidosCancelados)
+                    p.ExibirResumo();
                 break;
+
             case 7:
-                foreach (var item in clientes)
-                {
-                    System.Console.WriteLine(item.Nome + " " + item.Telefone);
-                }
-
-
+                foreach (var c in clientes)
+                    if(c != null)
+                        Console.WriteLine(c.Nome + " " + c.Telefone);
                 break;
+
             case 8:
                 CardapioService.CardapioPorGrupos(cardapio);
                 break;
+
             case 9:
+                Console.WriteLine("MATRIZ DE MESAS");
+                for(int i=0;i<5;i++)
+                {
+                    for(int j=0;j<5;j++)
+                        Console.Write(mesas[i,j] ? "[X]" : "[O]");
+                    Console.WriteLine();
+                }
+                break;
+
+            case 10:
+                Console.WriteLine("Pedidos ordenados por valor total:");
+                foreach(var p in filaPedido.OrderByDescending(x => x.Total))
+                    Console.WriteLine($"Código: {p.Codigo} | Cliente: {p.Cliente.Nome} | Total: {p.Total}");
+                break;
+
+            case 11:
                 rodar = false;
                 break;
+
             default:
                 Console.WriteLine("Opção inválida.");
                 break;
         }
     }
-    
-
-
-
-
 }
 catch(Exception e)
 {
